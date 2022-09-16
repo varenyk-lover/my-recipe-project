ea752dc39a79e7ade9ed80978457a1956f80bc30
hanna <rockyeaaah@gmail.com>
2022-09-16 17:09:46 +0300

    Changed logo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
-// import {Link} from 'react-router-dom';
 import {AppRoutes} from './routes/AppRoutes';
-/*import StartPageMainComponent from './components/StartPageMain/StartPageMainComponent';*/
+
+
 
 function App() {
+
     return (
         <div>
             <AppRoutes/>
